8e709fe260d8cac7b3569327e9c0b22d4da884ab
cpthgli <cpthgli@gmail.com>
2020-07-12 22:49:10 +0900

    v1.0

---
+++ screenclock/App.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Cysharp.Text;
+
+using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace screenclock
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var args = e.Args;
+            var wins = new List<Window>();
+
+            if (args.Length > 0)
+            {
+                if (args[0].StartsWith("/S", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    wins.AddRange(ScreenSaver());
+                }
+                else if (args[0].StartsWith("/C", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    wins.Add(new SettingsWindow());
+                }
+                else if (args[0].StartsWith("/P", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if (args.Length < 2)
+                    {
+                        var ex = new ArgumentException(ZString.Join(" ", args));
+                        _ = MessageBox.Show(ex.Message, ex.ParamName, MessageBoxButton.OK, MessageBoxImage.Error);
+                        throw ex;
+                    }
+
+                    //var previewWndHandle = new IntPtr(long.Parse(args[1], CultureInfo.InvariantCulture));
+                    //wins.Add(new ScreenSaverWindow(previewWndHandle));
+                }
+                else
+                {
+                    var ex = new ArgumentException(ZString.Join(" ", args));
+                    _ = MessageBox.Show(ex.Message, ex.ParamName, MessageBoxButton.OK, MessageBoxImage.Error);
+                    throw ex;
+                }
+            }
+            else
+            {
+                wins.Add(new SettingsWindow());
+            }
+
+            foreach (var win in wins)
+            {
+                win.Show();
+            }
+        }
+
+        private static List<Window> ScreenSaver()
+        {
+            var (path, blur) = LoadSettings();
+
+            var wins = new List<Window>();
+
+            foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            {
+                wins.Add(new ScreenSaverWindow(screen.Bounds, path, blur));
+            }
+
+            return wins;
+        }
+
+        private static (string, double) LoadSettings()
+        {
+            var settings = new Settings();
+            settings.LoadSettings();
+
+            if (!Directory.Exists(settings.Path))
+            {
+                return (string.Empty, settings.Blur);
+            }
+
+            var pngs = Directory.GetFiles(settings.Path, "*.png");
+            var jpgs = Directory.GetFiles(settings.Path, "*.jpg");
+            var jpegs = Directory.GetFiles(settings.Path, "*.jpeg");
+            var files = pngs.Concat(jpgs).Concat(jpegs).ToList();
+            var rand = new Random();
+            var path = files.Count == 0 ? string.Empty : files[rand.Next(0, files.Count - 1)];
+
+            return (path, settings.Blur);
+        }
+    }
+}
---
+++ screenclock/ScreenSaverWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace screenclock
+{
+    /// <summary>
+    /// ScreenSaver.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ScreenSaverWindow : Window, IDisposable
+    {
+        #region Win32 API functions
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern bool GetClientRect(IntPtr hWnd, out Rectangle lpRect);
+        #endregion
+
+        private Point? MouseLocation { get; set; }
+        private bool PreviewMode { get; set; }
+        private Timer Timer { get; set; }
+        private DateTime Time { get; set; }
+
+        public ScreenSaverWindow()
+        {
+            this.PreviewMode = false;
+            this.InitializeComponent();
+            this.Timer = new Timer();
+        }
+
+        public ScreenSaverWindow(System.Drawing.Rectangle bounds, string path, double blur)
+        {
+            this.PreviewMode = false;
+            this.InitializeComponent();
+
+            var dpi = VisualTreeHelper.GetDpi(this);
+
+            this.Width = bounds.Width / dpi.DpiScaleX;
+            this.Height = bounds.Height / dpi.DpiScaleY;
+            this.Left = bounds.Left / dpi.DpiScaleX;
+            this.Top = bounds.Top / dpi.DpiScaleY;
+
+            this.Image.Source = string.IsNullOrWhiteSpace(path) ? null : new BitmapImage(new Uri(path));
+            this.Blur.Radius = blur;
+
+            var now = DateTime.Now;
+            this.SetTime(now);
+            this.Time = now;
+
+            this.Timer = new Timer(100);
+            this.Timer.Elapsed += (_, e) =>
+            {
+                if (e.SignalTime.Hour != this.Time.Hour || e.SignalTime.Minute != this.Time.Minute)
+                {
+                    this.SetTime(e.SignalTime);
+                    this.Time = e.SignalTime;
+                }
+            };
+            this.Timer.Start();
+        }
+
+        private void SetTime(DateTime time)
+        {
+            var t = time.ToString("HHmm", CultureInfo.InvariantCulture).ToArray();
+            this.Dispatcher.Invoke(() =>
+            {
+                this.H1.Text = t[0].ToString();
+                this.H2.Text = t[1].ToString();
+                this.M1.Text = t[2].ToString();
+                this.M2.Text = t[3].ToString();
+            });
+        }
+
+        public ScreenSaverWindow(IntPtr PreviewWndHandle)
+        {
+            this.PreviewMode = true;
+            this.InitializeComponent();
+
+            var helper = new System.Windows.Interop.WindowInteropHelper(this);
+            _ = SetParent(helper.Handle, PreviewWndHandle);
+            _ = SetWindowLong(helper.Handle, -16, new IntPtr(GetWindowLong(helper.Handle, -16) | 0x40000000));
+            _ = GetClientRect(PreviewWndHandle, out var ParentRect);
+
+            this.Width = ParentRect.Width;
+            this.Height = ParentRect.Height;
+            this.Left = 0;
+            this.Height = 0;
+
+            this.Timer = new Timer(100);
+            this.Timer.Elapsed += (_, e) =>
+            {
+                if (e.SignalTime.Hour != this.Time.Hour || e.SignalTime.Minute != this.Time.Minute)
+                {
+                    this.SetTime(e.SignalTime);
+                    this.Time = e.SignalTime;
+                }
+            };
+            this.Timer.Start();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            var CurrentLocation = e.GetPosition(this);
+            if (!this.PreviewMode)
+            {
+                if (!(this.MouseLocation is null))
+                {
+                    var x2 = Math.Pow(this.MouseLocation.Value.X - CurrentLocation.X, 2);
+                    var y2 = Math.Pow(this.MouseLocation.Value.Y - CurrentLocation.Y, 2);
+                    if (Math.Sqrt(x2 + y2) > 3)
+                    {
+                        Application.Current.Shutdown();
+                    }
+                }
+
+                this.MouseLocation = CurrentLocation;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!this.PreviewMode) Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!this.PreviewMode) Application.Current.Shutdown();
+        }
+
+        public void Dispose()
+        {
+            this.Timer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
